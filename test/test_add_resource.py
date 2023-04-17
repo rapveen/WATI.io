16854d3883ca39ca7f1ba21c509094878fc27784
rapveen <raisingpraveen691@gmail.com>
2023-04-17 15:20:40 +0530

    Update unittest to use persistent file storage

--- test/test_add_resource.py
+++ test/test_add_resource.py
@@ -3,6 +3,7 @@ import unittest
 from app import app
 from falcon import testing
 from route_description import RouteName
+from constants import Constants
 
 class TestAddResource(unittest.TestCase):
     def setUp(self):
@@ -10,14 +11,18 @@ class TestAddResource(unittest.TestCase):
         self.test_client = testing.TestClient(self.app)
 
     def test_addition(self):
-        expected_result = 5
-        req_body = {"num1": 2, "num2": 3}
+        expected_result = 7
+        req_body = {"num1": 3, "num2": 4}
         expected_resp_body = {"result": expected_result}
 
         resp = self.test_client.simulate_post(RouteName.ADD, json=req_body)
 
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(json.loads(resp.content), expected_resp_body)
+
+        with open(Constants.PERSISTENCE_STORAGE_PATH, "r") as f:
+            contents = f.read()
+        self.assertIn("3,4,7", contents)
 
 
 if __name__ == '__main__':
